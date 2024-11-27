cc56d29a9aab089fea475c54a2a3a574b40df813
sskyy <skyking_H@hotmail.com>
2024-11-27 22:30:52 +0800

    feat: support locale query string

--- playground.tsx
+++ playground.tsx
@@ -1,8 +1,11 @@
 import {createElement, createRoot} from "axii";
 import {Playground} from "./src/Playground";
 
+// 从 url query string 中读取 locale
+const urlParams = new URLSearchParams(window.location.search);
+const locale = urlParams.get('locale') || 'en'
 
 const root = document.getElementById('root')!
 const appRoot = createRoot(root)
-appRoot.render(<Playground/>)
+appRoot.render(<Playground locale={locale}/>)
 console.log('rendering.....')
